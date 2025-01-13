b2dcc0ae7d1646cbd46ac9cf148ea4d45f579320
Lapraniteon <lapraniteon@gmail.com>
2025-01-13 16:48:57 +0100

    Add Margin Check

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,7 +9,7 @@ public class CameraController : MonoBehaviour
     public float cameraMoveSpeed;
 
     [Space] 
-    [Tooltip("The area around the screen in which the mouse can be to move around, in pixels.")]
+    [Tooltip("The area around the screen in which the mouse can be to move around, in a fraction from 0 - 1")]
     public float movementAreaMargin;
     
     // Start is called before the first frame update
@@ -21,8 +21,20 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CameraMovement();
+    }
+
+    private void CameraMovement()
+    {
         float xPositionNormalized = Input.mousePosition.x / Screen.width;
         float yPositionNormalized = Input.mousePosition.y / Screen.height;
+        
+        if (xPositionNormalized >= movementAreaMargin && xPositionNormalized <= 1f - movementAreaMargin)
+            if (yPositionNormalized >= movementAreaMargin && yPositionNormalized <= 1f - movementAreaMargin)
+                return;
+
+        float xMagnitude = xPositionNormalized / 2f + 1f;
+        float yMagnitude = yPositionNormalized / 2f + 1f;
 
         Debug.Log(xPositionNormalized + ", " + yPositionNormalized);
     }
