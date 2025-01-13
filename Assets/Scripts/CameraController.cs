f2793fdc4209e76f9c41a8cd06b5ebfd918b559c
Lapraniteon <lapraniteon@gmail.com>
2025-01-13 16:39:01 +0100

    Get Mouse Position

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+    [Tooltip("How fast the camera will move around.")]
+    public float cameraMoveSpeed;
+
+    [Space] 
+    [Tooltip("The area around the screen in which the mouse can be to move around, in pixels.")]
+    public float movementAreaMargin;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,9 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xPositionNormalized = Input.mousePosition.x / Screen.width;
+        float yPositionNormalized = Input.mousePosition.y / Screen.height;
+
+        Debug.Log(xPositionNormalized + ", " + yPositionNormalized);
     }
 }
