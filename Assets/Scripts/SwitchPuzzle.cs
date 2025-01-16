ba95cbe11a4b629a22b652809ff0405bf3e51bb1
Lapraniteon <lapraniteon@gmail.com>
2025-01-16 14:05:35 +0100

    Script Switch puzzle

---
+++ Assets/Scripts/SwitchPuzzle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchPuzzle : MonoBehaviour
+{
+    [Tooltip("The name of the layer that the pipe segments are on.")]
+    public string switchButtonLayer;
+    private LayerMask _switchButtonLayerMask;
+
+    [Tooltip("Is this puzzle solved?")]
+    public bool isSolved = false;
+
+    public UnityEvent m_OnSolved;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, Mathf.Infinity, _switchButtonLayerMask, QueryTriggerInteraction.Collide))
+            {
+                if (!isSolved)
+                    isSolved = true;
+
+                GameManager.Instance.solved_firstSwitch++;
+                m_OnSolved.Invoke();
+            }
+        }
+    }
+}
