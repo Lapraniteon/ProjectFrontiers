a9998816a5c94ecd26d3ec2de65e820e9d45cb6b
Lapraniteon <lapraniteon@gmail.com>
2025-01-17 10:48:04 +0100

    Add variables + turn on the lights method

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 // Global Game Manager class to handle game logic and global variables.
@@ -22,13 +23,27 @@ public class GameManager : MonoBehaviour
     [Tooltip("Whether the camera is currently being prevented from moving by a target.")]
     public bool cameraIsLocked;
 
+    public bool keypadUnlocked;
+
+    public bool lightsTurnedOn;
+
     public int solved_firstSwitch;
     public int solved_keypadPuzzle;
     public int solved_wirePuzzle;
     public int solved_pipePuzzle;
 
+    [Space]
+    [Header("Events")]
+    public UnityEvent m_OnPuzzleCompletion;
+
     void Awake()
     {
         _instance = this;
+    }
+
+    public void TurnOnLights()
+    {
+        if (!lightsTurnedOn)
+            return;
     }
 }
