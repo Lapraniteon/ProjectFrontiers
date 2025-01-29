e8c550e7a253f99024beb0b28b12ca24627ff51f
Jacqueline Christina Boesiger <jacqueline@bosiger.com>
2025-01-29 14:34:16 +0100

    Added Lights

--- Assets/Scripts/IsWirePuzzelSolved.cs
+++ Assets/Scripts/IsWirePuzzelSolved.cs
@@ -11,6 +11,8 @@ public class IsWirePuzzelSolved : MonoBehaviour
 
     public UnityEvent m_OnSolved;
 
+    public GameObject lights;
+
     public bool CheckSolution()
     {
         for (int i = 0; i < wireConnected.Length; i++)
@@ -18,7 +20,7 @@ public class IsWirePuzzelSolved : MonoBehaviour
             if (wireConnected[i].GetComponent<DragAndDrop>().wiresConnected == false)
                 return false;
         }
-
+        lights.SetActive(true);
         GameManager.Instance.solved_wirePuzzle++;
         m_OnSolved.Invoke();
         return true;
